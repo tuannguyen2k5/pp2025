92e33d3c1508e7ff67f1a413ea527ffebf0a4dfc
tuannguyen2k5 <tuanrose1422005@gmail.com>
2025-12-04 22:46:17 +0700

    submit

---
+++ 1.student.mark.py
@@ -0,0 +1,97 @@
+students = []
+courses = []
+marks = {} #{course_id: {student_id: mark}}
+
+# STUDENTS
+def information_student():
+    student_id = input("enter the student id: ")
+    student_name = input("Enter the student name: ")
+    student_Dob = input("enter date of birth: ")
+
+    students.append({
+        "id" : student_id, 
+        "name": student_name, 
+        "Dob": student_Dob
+    })
+def input_student():
+    n = int(input("enter the number of students in a class: "))
+    for i in range(n):
+        print(f"\n--- Student {i+1} ---")
+        information_student()
+    print("\nStudent input successfully!!")
+# =========
+#COURSE
+def information_course():
+    course_id = input("enter course ID: ")
+    course_name = input("enter course name: ")
+
+    courses.append({
+        "id": course_id,
+        "name": course_name
+    })
+def input_course():
+    n = int(input("\nEnter number of the courses: "))
+    for i in range(n):
+        print(f"\n--- course {i+1} ---")
+        information_course()
+    print("\nCourses input successfully!!")
+
+# Marks
+
+def input_marks():
+    print("\nAvailable courses: ")
+    for i, c in enumerate(courses): # vòng lặp for sử dụng enumerate()
+        print(f"{i+1}.{c['name']} (ID: {c['id']})")
+
+    choice = int(input("\nSelect a course by number: ")) - 1
+    course_id = courses[choice]["id"]
+    """
+    courses là list các dictionary
+    courses[choice] lấy ra 1 dictionary
+    ["id"] truy cập vào key “id”
+    """
+    marks[course_id] = {}
+
+    print(f"\nEnter marks for course: {courses[choice]['name']}")
+    for s in students:
+        score = float(input(f"Enter mark for {s['name']} (ID: {s['id']}): "))
+        marks[course_id][s["id"]]= score
+    print("\nMarks input successfully!")
+
+# Listing functions
+def list_students():
+    print("\n=== STUDENT LIST ===")
+    for s in students:
+        print(f"ID: {s['id']}, Name: {s['name']}, DOB: {s['Dob']}")
+
+def list_courses():
+    print("\n=== COURSE LIST ===")
+    for c in courses:
+        print(f"ID: {c['id']}, Name: {c['name']}")
+def show_marks():
+    print("\nAvaiable courses:")
+    for i, c in enumerate(courses):
+        print(f"{i+1}. {c['name']} (ID: {c['id']})")
+
+    choice = int(input("\nSelect a course to view marks: ")) - 1
+    course_id = courses[choice]["id"]
+
+    print(f"\n=== Marks for course: {courses[choice]['name']} ===")
+
+    if course_id not in marks:
+        print("No marks recorded for this course.")
+        return
+    for s in students:
+        s_id = s["id"]
+        score = marks[course_id].get(s_id, "N/A")
+        print(f"{s['name']} ({s_id}): {score}")
+
+def main():
+    input_student()
+    input_course()
+    input_marks()
+    list_students()
+    list_courses()
+    show_marks()
+
+main()   
